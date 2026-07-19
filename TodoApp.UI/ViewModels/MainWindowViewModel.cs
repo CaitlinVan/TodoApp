66f0b875ca45d4f97273bdac4f9f69af8bbf4a83
CaitlinVan <0caitlinvan0@gmail.com>
2026-07-19 14:55:11 +0200

    Edits in ViewModel folder

--- TodoApp.UI/ViewModels/MainWindowViewModel.cs
+++ TodoApp.UI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace TodoApp.UI.ViewModels;
+﻿using TodoApp.Console.Data;
+using TodoApp.Console.Domain; 
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace TodoApp.UI.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    private readonly ITaskRepository _taskRepository;
+    
+    [ObservableProperty]
+    private ObservableCollection<TodoTask> _tasks = new();
+
+    public MainWindowViewModel()
+    {
+        
+    }
+    
+    
 }
---
+++ TodoApp.UI/ViewModels/TaskListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using TodoApp.Console.Domain;
+
+namespace TodoApp.UI.ViewModels;
+
+public class TaskListViewModel
+{
+    public ObservableCollection<TodoTask> Tasks { get; set; } = new();
+    
+    private readonly ITaskRepository _repository;
+
+    public TaskListViewModel(ITaskRepository repository)
+    {
+        _repository =  repository;
+    }
+
+    public async Task LoadTasksAsync()
+    {
+        var tasks = await _repository.GetAllAsync();
+        Tasks =  new ObservableCollection<TodoTask>(tasks);
+    }
+}
